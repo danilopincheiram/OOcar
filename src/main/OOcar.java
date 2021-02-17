67f6a007ddcfce8dc5da4850e1ac272bd604dde3
danilopincheira <danilopincheira@users.noreply.github.com>
2021-02-17 19:27:18 +0000

    Initial commit

---
+++ src/main/OOcar.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+import model.car;
+import model.person;
+
+/**
+ *
+ * @author Danilo Pincheira
+ */
+public class OOcar {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    // instantiation of a person
+    person p1 = new person("juan", 28, "male");
+    
+    person p2 = new person("mario", 30, "male");
+     // instantiation of a car
+    car car1 = new car();
+     // setOwner, not print
+     car1.setOwner(p1);
+      // print the lines
+        System.out.println(car1);
+    
+      //instantiaton of car 2
+    car car2 = new car("Dodge", "challenger", 2020, "black");
+        // setOwner
+         car2.setOwner(p2);
+       //print toString with the owner added
+        System.out.println(car2);
+    
+     // System.out.println(car2.toString());
+    }  
+}
---
+++ src/model/car.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Danilo Pincheira
+ */
+public class car {
+   // properties 
+    private String brand;
+    private String model;
+    private int year;
+    private String colour;
+    protected person owner;  
+     
+    //default constructor
+    public car (){
+        this.brand = "toyota";       
+        this.model = "auris";
+        this.year = 2020;
+        this.colour = "red";
+        this.owner = null;
+    }
+    //constructor
+   public car(String brand, String model,int year, String colour){
+       this.brand = brand;
+       this.model = model;
+       this.year = year;
+       this.colour = colour;
+
+       
+   } 
+   //methods
+   public String Running(){
+       return "the " + this.brand + ", " + this.brand + " is going very quick";
+   }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+    public person getOwner() {
+        return owner;
+    }
+
+    public void setOwner(person owner) {
+        this.owner = owner;
+    }
+    
+    @Override
+   public String toString(){
+       return "the car "  + this.brand + ", " + this.model + ", " + "year " + this.year + ", " + " colour " + this.colour + ", owner: " + this.owner.getName();
+   }
+}
